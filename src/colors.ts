3b6224c2cce69e951c39e96b6729cf8050d5ccfd
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-05-28 18:13:58 +0100

    fix: remove unused variables

--- src/colors.ts
+++ src/colors.ts
@@ -3,44 +3,35 @@ import { colors } from '@primer/primitives';
 
 const dark: IColors = {
     cursor: colors.blue[2],
-    foreground: '#fafbfc',
+    foreground: colors.gray[0],
     background: '#1f2428',
     comments: '#637777',
     black: '#1b1f23',
     white: '#fff',
-    gray: "#fafbfc",
-    blue: '#79b8ff',
-    green: '#85E89D',
-    yellow: '#FFFDEF',
-    orange: '#ffab70',
-    red: '#F97583',
-    purple: '#b392f0',
-    deprecated: '#b31d28',
-    invalid: '#b31d28',
-    diffAdded: '#addb67',
-    diffModified: '#e2b93d',
-    diffDeleted: '#ef5350',
+    gray: colors.gray[0],
+    blue: colors.blue[3],
+    green: colors.green[3],
+    yellow: colors.yellow[0],
+    orange: colors.orange[3],
+    red: colors.red[3],
+    purple: colors.red[3],
+    pink: colors.red[3]
 };
 
 const light: IColors = {
     cursor: colors.blue[7],
-    foreground: '#24292E',
+    foreground: colors.gray[9],
     background: "#fff",
-    comments: '#6A737D',
+    comments: colors.gray[5],
     black: '#1b1f23',
     white: '#fff',
-    gray: "#24292e",
-    blue: '#005CC5',
+    gray: colors.gray[9],
+    blue: colors.blue[6],
     green: '#22863A',
     yellow: '#735C0F',
     orange: '#E36209',
     red: '#D73A49',
-    purple: '#6F42C1',
-    deprecated: '#b31d28',
-    invalid: '#b31d28',
-    diffAdded: '#addb67',
-    diffModified: '#e2b93d',
-    diffDeleted: '#ef5350',
+    purple: '#6F42C1'
 };
 
 export { light, dark };
