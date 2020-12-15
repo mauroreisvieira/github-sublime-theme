70dfef99600fc90de831847d4122a1729f81cbe3
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-12-15 15:20:29 +0000

    chore: update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,31 +1,30 @@
 set -e
-echo "Current version:" $(grep version package.json | sed -E 's/^.*"(4[^"]+)".*$/\1/')
+echo "Current version: "$(grep version package.json | sed -E 's/^.*"(4[^"]+)".*$/\1/')
 echo "Enter release version: "
 read VERSION
 
-read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
+read -p "Releasing 3179-$VERSION - are you sure? (y/n)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing v$VERSION ..."
-  yarn run lint
+  echo "Releasing 3179-$VERSION ..."
+  npm run lint
 
-  yarn run build
-  # generate the version so that the changelog can be generated too
-  yarn version --no-git-tag-version --no-commit-hooks --new-version $VERSION
+  npm run build
+  # GENERATE THE VERSION SO THAT THE CHANGELOG CAN BE GENERATED TOO
+  npm version --no-git-tag-version --no-commit-hooks --new-version $VERSION
 
-  # changelog
-  yarn run changelog
+  # CHANGELOG
+  npm run changelog
 
-  echo "Please check the git history and the changelog and press enter"
+  echo "Please check the git history and the CHANGELOG and press enter"
   read OKAY
 
-  # commit and tag
+  # COMMIT AND TAG
   git add CHANGELOG.md package.json
-  git commit -m "chore(release): v$VERSION"
-  git tag -a "v$VERSION" -m $VERSION
+  git commit -m "chore(release): 3179-$VERSION"
+  git tag -a "3179-$VERSION" -m "3179-$VERSION"
 
-  # publish
-  git push origin refs/tags/v$VERSION
+  # PUBLISH
+  git push origin refs/tags/3179-$VERSION
   git push
 fi
